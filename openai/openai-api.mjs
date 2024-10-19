9fd38d374b9b72a6ce64589918444c0c33b4c8ce
dchuquilla <dario.chuquilla@gmail.com>
2024-10-18 20:11:15 -0500

    Refactor code to improve audio transcription and payload creation

--- openai/openai-api.mjs
+++ openai/openai-api.mjs
@@ -5,7 +5,7 @@ import axios from "axios";
 const openai = new OpenAI();
 
 export async function transcribeAudio(url, audio_id) {
-  console.log("transcribing audio")
+  console.log("Transcribing audio: ", url, audio_id)
   const filePath = `./files/${audio_id}.ogg`;
 
   // Download the file
@@ -25,25 +25,25 @@ export async function transcribeAudio(url, audio_id) {
       writer.on('error', reject);
   });
 
+  // Send audio to OpenAI and transcribe
   const transcription = await openai.audio.transcriptions.create({
     file: fs.createReadStream(filePath),
     model: "whisper-1",
   });
 
-  console.log(transcription.text);
-
-  create_payload(transcription.text)
+  console.log("Transcription: ", transcription.text)
+  return transcription.text;
 }
 
 export async function create_payload(text) {
-  console.log("creating payload")
+  console.log("Creating payload for text: ", text)
   try {
     const payload = await new Promise((resolve, reject) => {
       openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
         { role: "system", content: "Eres un experto en JSON. Por favor crea un objeto JSON siguiendo estas reglas" },
-        { role: "system", content: "ESTRUCTURA: {\"request\": { \"replacement\": \"string\", \"brand\": \"string\", \"model\": \"string\", \"year\": \"string\" }}." },
+        { role: "system", content: "ESTRUCTURA: {\"message\": \"accepted\",\"request\": { \"replacement\": \"string\", \"brand\": \"string\", \"model\": \"string\", \"year\": \"string\" }}." },
         { role: "system", content: "SALUDO: Cuando el texto sea un saludo sin informacion de alguna solicitud, responde con {\"message\": \"greeting\"}" },
         { role: "system", content: "EXCEPTION: Cuando el texto no sea un saludo o una solicitud de repuesto, responde con {\"message\": \"not_replacement_request\"}" },
         {
@@ -53,7 +53,6 @@ export async function create_payload(text) {
       ],
       }).then(resolve).catch(reject);
     });
-    console.log("Payload 1: ", payload.choices[0].message.content)
     return JSON.parse(payload.choices[0].message.content);
   } catch (error) {
     console.log("Error: ", error)
