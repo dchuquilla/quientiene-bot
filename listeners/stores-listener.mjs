654ac48a7a5d53a21814d8acdb7e4ec26f9f1091
dchuquilla <dario.chuquilla@gmail.com>
2024-11-09 12:16:31 -0500

    ✨ Add feature/xxx
    📝 A brief summary of the changes made
    
    📊 List of changes made in bullet points:
    - 🐛 Bug fix:
      - [ ] Change 1
      - [ ] Change 2
    - 🌟 Enhancement:
      - [ ] Change 1
      - [ ] Change 2
    - 📦 Dependency upgrade:
      - [ ] Change 1
      - [ ] Change 2
    - 🗑️ Deprecation:
      - [ ] Change 1
      - [ ] Change 2
    
    🔗 Related Issues:
    - [CAB-XXX]:
    
    👥 Reviewers:
    - None

---
+++ listeners/stores-listener.mjs
@@ -0,0 +1,17 @@
+import { sendWhapiRequest } from "../whapi/whapi-manager.mjs";
+import { setupSnapshotListener } from "../firebase/fb-initializer.mjs";
+
+export default function StoresListener() {
+  return setupSnapshotListener("stores", async (store) => {
+  // send message to the phone phone in store object
+  const endpoint = 'messages/text';
+  const phone = store.phone.replace(/\D/g, ''); // Remove non-numeric characters
+  const sender = {
+    to: `${phone}@s.whatsapp.net`,
+    body: `😊 La cuenta de *${store.name}* fue creada con éxito.`,
+  }
+  sender.body
+  // eslint-disable-next-line
+  await sendWhapiRequest(endpoint, sender);
+});
+}
